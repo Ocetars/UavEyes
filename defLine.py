9943cb9a043823f3d062d62dcb8971f85e8d7ee3
Ocetars <ocetars@gmail.com>
2023-04-21 18:39:20 +0800

    格式化

--- defLine.py
+++ defLine.py
@@ -1,13 +1,16 @@
 import cv2
 import numpy as np
-import math
+
 
 # 定义一个函数，用于计算直线和画面中心的距离
 def distance_to_center(line):
     # 获取直线的端点坐标
     x1, y1, x2, y2 = line[0]
     # 获取画面的中心坐标
-    cx, cy = cap.get(cv2.CAP_PROP_FRAME_WIDTH) / 2, cap.get(cv2.CAP_PROP_FRAME_HEIGHT) / 2
+    cx, cy = (
+        cap.get(cv2.CAP_PROP_FRAME_WIDTH) / 2,
+        cap.get(cv2.CAP_PROP_FRAME_HEIGHT) / 2,
+    )
     # 计算直线的向量
     vx, vy = x2 - x1, y2 - y1
     # 计算中心点到直线的向量
@@ -15,24 +18,24 @@ def distance_to_center(line):
     # 计算向量的叉积
     cross = abs(vx * wy - vy * wx)
     # 计算直线的长度
-    length = np.sqrt(vx ** 2 + vy ** 2)
+    length = np.sqrt(vx**2 + vy**2)
     # 返回点到直线的距离
     return cross / length
 
+
 # 定义一个函数，用于检测最靠近中心的直线
-def detect_line(image,
-                rho = 1,
-                theta = np.pi/180, 
-                threshold = 150, 
-                minLineLength = 50, 
-                MaxValue = 1000):
+def detect_line(
+    image, rho=1, theta=np.pi / 180, threshold=150, minLineLength=50, MaxValue=1000
+):
     # 转换为灰度图像
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # 检测边缘
     edges = cv2.Canny(gray, 100, 250, apertureSize=3, L2gradient=True)
     cv2.imshow("edges", edges)
     # 检测直线
-    lines = cv2.HoughLinesP(edges, rho, theta, threshold, minLineLength, maxLineGap=MaxValue)
+    lines = cv2.HoughLinesP(
+        edges, rho, theta, threshold, minLineLength, maxLineGap=MaxValue
+    )
     # 如果检测到直线，返回最靠近中心的直线的端点坐标和距离
     if lines is not None:
         # 初始化最小距离和最小距离对应的直线索引
@@ -53,6 +56,9 @@ def detect_line(image,
         # 如果没有检测到直线，返回None
         return None
 
+
+"""以下是主程序，仅用于测试函数"""
+
 # 创建视频捕捉对象
 cap = cv2.VideoCapture(0)
 
@@ -69,7 +75,7 @@ while True:
     if result is not None:
         (x1, y1), (x2, y2), min_dist = result
         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        print(f"({x1}, {y1}), ({x2}, {y2}), {min_dist}")     
+        print(f"({x1}, {y1}), ({x2}, {y2}), {min_dist}")
     elif result is None:
         print("line none")
     else:
