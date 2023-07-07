6dc1f6054370410b575bae876d3c4bf85fa0976e
Ocetars <ocetars@gmail.com>
2023-07-07 22:13:07 +0800

    新增国赛题目py

--- qr_bar_all_func.py
+++ qr_bar_all_func.py
@@ -19,7 +19,7 @@ def detect_qrcode(frame):
 
 def detect_barcode(frame):
     # 创建条形码检测器
-    detect_obj = cv2.barcode_BarcodeDetector()
+    detect_obj = cv2.barcode_BarcodeDetector()  # type: ignore
 
     # 检测条形码
     is_ok, bar_info, bar_type, points = detect_obj.detectAndDecode(frame)
@@ -51,7 +51,7 @@ def detect_allcode(frame):
     # 设置二维码检测器
     qrcoder = cv2.QRCodeDetector()
     # 创建条形码检测器
-    detect_obj = cv2.barcode_BarcodeDetector()
+    detect_obj = cv2.barcode_BarcodeDetector() # type: ignore
 
     # 检测识别二维码
     codeinfo, QRpoints, straight_qrcode = qrcoder.detectAndDecode(frame)
